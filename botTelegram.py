c808baea387d2a5562de0c14e5b4ce906e4b0a68
LeWal12 <59091635+LeWal12@users.noreply.github.com>
2020-04-09 23:40:25 -0300

    Add files via upload

---
+++ botTelegram.py
@@ -0,0 +1,21 @@
+from telegram.ext import Updater, CommandHandler
+
+def welcome(update, context):
+	message = 'Ola, ' + update.message.from_user.first_name + '!'
+	print(message)
+	context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+def main():
+	token = '793256335:AAGDOlaPbGeCOastdaWtuufhIlvrHZjg32s'
+	updater = Updater(token=token, use_context=True)
+
+	updater.dispatcher.add_handler(CommandHandler('start', welcome))
+
+	updater.start_polling()
+	print('testando bot' + str(updater))
+	updater.idle()
+
+if __name__ == '__main__':
+	main()
+
